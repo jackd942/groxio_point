e31d218bb5dd947e28ce00e8f5815ed35522f743
Jack Davis <jackd942@gmail.com>
2020-05-07 15:54:38 -0500

    initial commit

---
+++ lib/point.ex
@@ -0,0 +1,18 @@
+defmodule Point do
+  @moduledoc """
+  Documentation for `Point`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Point.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
